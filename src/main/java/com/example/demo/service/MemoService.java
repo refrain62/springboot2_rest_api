b6698e845feca77771bad04b870d458292e624bd
nao <nao_h13@hotmail.com>
2021-06-08 01:25:45 +0900

    https://github.com/rubytomato/demo-security-spring2 の写経

---
+++ src/main/java/com/example/demo/service/MemoService.java
@@ -0,0 +1,20 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Memo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
+public interface MemoService
+{
+  Optional<Memo> findById(Long id);
+
+  Page<Memo> findAll(Pageable page);
+
+  void store(Memo memo);
+
+  void updateById(Long id, Memo memo);
+
+  void removeById(Long id);
+}
---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,10 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.User;
+
+import java.util.Optional;
+
+public interface UserService
+{
+    Optional<User> findByName(String name);
+}
